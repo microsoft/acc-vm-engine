4e8302ec06649c7d92eb36feed0b8b327f1f0a1d
Anjuli Goring <anjuligoring@gmail.com>
2021-08-24 11:45:51 -0700

    unused variable

--- pkg/api/validate.go
+++ pkg/api/validate.go
@@ -61,7 +61,7 @@ func (p *Properties) validateVMProfile(vmconf VMConfigurator) error {
 	if len(vm.OSType) == 0 {
 		return fmt.Errorf("OSType is not specified")
 	}
-	hasOsImage := (len(vm.OSName) > 0 || vm.OSImage != nil || len(vm.OSImageName) > 0)
+// 	hasOsImage := (len(vm.OSName) > 0 || vm.OSImage != nil || len(vm.OSImageName) > 0)
 	hasOsDisk := (vm.OSDisk != nil)
 
 // 	if hasOsImage {
